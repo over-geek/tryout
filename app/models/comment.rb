86a342c1cf7cbd40cccf3ea5e81bb061c6818492
over-geek <kenagy100@gmail.com>
2024-01-11 18:54:31 +0000

    Add methods to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user, foreign_key: 'author_id'
+
+  after_save :update_comments_counter
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,11 @@
 # Represents a like associated with a post and a user.
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: 'author_id'
   belongs_to :post
+
+  after_save :update_likes_counter
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
