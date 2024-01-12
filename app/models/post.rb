10a7bbd9723b7718363a13cb7238b0c8800e6e89
over-geek <kenagy100@gmail.com>
2024-01-12 01:07:41 +0000

    Fix likes_counters typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,11 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counters, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_initialize do |post|
     post.comments_counter ||= 0
-    post.likes_counter ||= 0
+    post.likes_counters ||= 0
   end
 
   def recent_comments
