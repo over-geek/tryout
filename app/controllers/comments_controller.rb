dc23aa90a4a0f51f0077471a6b701eb39f42af25
over-geek <kenagy100@gmail.com>
2024-01-20 12:14:32 +0000

    Modify comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
+    @user = @post.author
     @comment = Comment.new
   end
 
   def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params.merge(post: @post))
+    @comment.author = @post.author
 
     if @comment.save
       flash[:success] = 'Comment successfully added! 😎'
       redirect_to user_post_path(@post.author, @post)
     else
       flash[:alert] = 'Comment not added! 😢'
+      puts @comment.errors.full_messages # This will print validation errors to the console
       render :new, status: :unprocessable_entity
     end
   end
