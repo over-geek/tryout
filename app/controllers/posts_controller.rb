395af066205293b26386fc5dac60df6ed15aedee
over-geek <kenagy100@gmail.com>
2024-01-20 12:15:58 +0000

    Add set_user method to set current user to post author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_user
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc)
@@ -11,17 +13,28 @@ class PostsController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
     @post = Post.new
   end
 
   def create
-    @post = current_user.posts.new(post_params)
+    @post = @user.posts.new(post_params)
     if @post.save
       flash[:success] = 'Post created successfully😎!'
-      redirect_to "/users/#{current_user.id}/posts"
+      redirect_to user_posts_path(@user)
     else
       flash[:alert] = 'Post not created😢!'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
