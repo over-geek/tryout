b6e971ee8e0074d8f31158c4dcc1f004a58dd372
over-geek <kenagy100@gmail.com>
2024-01-26 12:50:15 +0000

    Add tests for see all post button and show recent post of user

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe 'Visit the user show page', type: :feature do
     @post1 = Post.create(author: @user, title: 'Post 1', text: 'This is the content of Post 1')
     @post2 = Post.create(author: @user, title: 'Post 2', text: 'This is the content of Post 2')
     @post3 = Post.create(author: @user, title: 'Post 3', text: 'This is the content of Post 3')
+    @post4 = Post.create(author: @user, title: 'Post 4', text: 'This is the content of Post 4')
+    @post5 = Post.create(author: @user, title: 'Post 5', text: 'This is the content of Post 5')
+    @post6 = Post.create(author: @user, title: 'Post 6', text: 'This is the content of Post 6')
   end
 
   it 'displays user information on the show page' do
@@ -17,29 +20,35 @@ RSpec.describe 'Visit the user show page', type: :feature do
 
     expect(page).to have_selector('img[src=\'https://randomuser.me/api/portraits/men/1.jpg\']')
     expect(page).to have_content 'John Doe'
-    expect(page).to have_content 'Number of posts: 3'
+    expect(page).to have_content 'Number of posts: 6'
     expect(page).to have_content 'A passionate individual with interesting stories to share.'
   end
 
   it 'displays the first 3 posts of the user' do
     visit user_path(@user)
 
-    expect(page).to have_content 'Post 1'
-    expect(page).to have_content 'Post 2'
-    expect(page).to have_content 'Post 3'
+    # Expect the first 3 posts to be displayed
+    expect(page).to have_content 'Post 4'
+    expect(page).to have_content 'Post 5'
+    expect(page).to have_content 'Post 6'
+
+    # Ensure only the three most recent posts are displayed
+    expect(page).not_to have_content 'Post 1'
+    expect(page).not_to have_content 'Post 2'
+    expect(page).not_to have_content 'Post 3'
   end
 
-  it 'redirects to the post show page when clicking a user\'s post' do
-    visit user_path(@user)
-
-    click_link 'Post 1'
-    expect(page).to have_current_path(user_post_path(@user, @post1))
-  end
-
+  # Ensure that user is redirected to post index page to see all posts when see all posts button is clicked
   it 'redirects to the user\'s posts index page when clicking to see all posts' do
     visit user_path(@user)
 
     click_link 'See All Posts'
     expect(page).to have_current_path(user_posts_path(@user))
+    expect(page).to have_content 'Post 4'
+    expect(page).to have_content 'Post 5'
+    expect(page).to have_content 'Post 6'
+    expect(page).to have_content 'Post 3'
+    expect(page).to have_content 'Post 2'
+    expect(page).to have_content 'Post 1'
   end
 end
