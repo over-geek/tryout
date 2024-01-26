a32d93e3a26fffcc6b0395a00b3ac7921162f8ef
over-geek <kenagy100@gmail.com>
2024-01-26 12:51:37 +0000

    display only 5 recent comments per post

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe 'Visit the user post index page', type: :feature do
     @post2 = Post.create(author: @user, title: 'Post 2', text: 'This is the content of Post 2')
     @comment1 = Comment.create(post: @post1, author: @user, text: 'First comment on Post 1')
     @comment2 = Comment.create(post: @post2, author: @user, text: 'First comment on Post 2')
+    @comment3 = Comment.create(post: @post2, author: @user, text: 'Second comment on Post 2')
+    @comment4 = Comment.create(post: @post2, author: @user, text: 'Third comment on Post 2')
+    @comment5 = Comment.create(post: @post2, author: @user, text: 'Fourth comment on Post 2')
+    @comment6 = Comment.create(post: @post2, author: @user, text: 'Fifth comment on Post 2')
+    @comment7 = Comment.create(post: @post2, author: @user, text: 'Sixth comment on Post 2')
     @like1 = Like.create(post: @post1, user: @user)
   end
 
@@ -25,7 +30,7 @@ RSpec.describe 'Visit the user post index page', type: :feature do
     expect(page).to have_content 'Post 2'
     expect(page).to have_content 'This is the content of Post 2'
     expect(page).to have_content 'First comment on Post 1'
-    expect(page).to have_content 'First comment on Post 2'
+    expect(page).to have_content 'Second comment on Post 2'
     expect(page).to have_content 'Comments: 1'
     expect(page).to have_content 'Likes: 1'
   end
@@ -44,5 +49,15 @@ RSpec.describe 'Visit the user post index page', type: :feature do
     visit user_posts_path(@user)
 
     expect(page).to have_content('Create new post')
+  end
+
+  it 'displays the first five comments on a post' do
+    visit user_posts_path(@user)
+    expect(page).to have_content 'Sixth comment on Post 2'
+    expect(page).to have_content 'Fifth comment on Post 2'
+    expect(page).to have_content 'Fourth comment on Post 2'
+    expect(page).to have_content 'Third comment on Post 2'
+    expect(page).to have_content 'Second comment on Post 2'
+    expect(page).not_to have_content 'First comment on Post 2'
   end
 end
